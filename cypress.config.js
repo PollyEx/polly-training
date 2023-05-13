6b270ed4470bec5b13604eeb7dd18db3015ea903
Isabelle Tennant <itennant@pollyex.com>
2023-05-12 17:34:56 -0700

    Training repo base

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,13 @@
-const { defineConfig } = require('cypress');
-const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
-const addCucumberPreprocessorPlugin =
-  require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
-const createEsbuildPlugin =
-  require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 5000,
-  pageLoadTimeout: 5000,
+  defaultCommandTimeout: 3000,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  videoCompression: 51,
   e2e: {
-    async setupNodeEvents(on, config) {
-      const bundler = createBundler({
-        plugins: [createEsbuildPlugin(config)],
-      });
-      on('task', {
-        log: message => {
-          console.log(message);
-          return null;
-        }
-      }),
-      on('file:preprocessor', bundler);
-      await addCucumberPreprocessorPlugin(on, config);
-
-      return config;
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
-    specPattern: 'cypress/e2e/features/**/*.feature',
-    baseUrl: 'http://127.0.0.1:8000',
-    chromeWebSecurity: false,
-    viewportHeight:1000,
   },
-  env: {
-    user: '',
-    password: '',
-  }
 });
