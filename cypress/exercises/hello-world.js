587eb1d01b4a42bafd21192ab1270ebc4b36028d
JakeBradenPolly <jbraden@pollyex.com>
2024-06-20 18:56:57 -0400

    test: example exercise

---
+++ cypress/exercises/hello-world.js
@@ -0,0 +1,7 @@
+// Step 1: Visit https://www.demoblaze.com/index.html and verify the "Home" [nav link] is visible with correct text
+
+// Step 2: Verify the "Contact" [nav link] is visible with correct text
+
+// Step 3: Verify the "About Us" [nav link] is visible with correct text
+
+// Step 4: Verify the "Cart" [nav link] is visible with correct text
